76b2776bb0b2aeb3c44f62de05e08f377683fa10
rg <83681395+rjzxysfwyt@users.noreply.github.com>
2022-03-26 21:12:55 +0800

    Update test.py

--- 聊天小程序/test.py
+++ 聊天小程序/test.py
@@ -4,10 +4,11 @@ def test():
     #测试服务器连接、数据收发
     client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     client_socket.connect(('127.0.0.1',8090))
-
-    client_socket.send('哈哈哈'.encode('utf-8'))
-    recv_data=client_socket.recv(512)
-    print(recv_data.decode('utf-8'))
+    while True:
+        msg=input('请输入：')
+        client_socket.send(msg.encode('utf-8'))
+    #     recv_data=client_socket.recv(512)
+    #     print(recv_data.decode('utf-8')) 
 
     client_socket.close()
 if __name__=='__main__':
