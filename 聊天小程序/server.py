ecfa5d0eba25a41e499dbbca26b362adfc8c6048
rg <83681395+rjzxysfwyt@users.noreply.github.com>
2022-03-26 21:12:20 +0800

    Update server.py

--- 聊天小程序/server.py
+++ 聊天小程序/server.py
@@ -1,5 +1,7 @@
 import socket
 from server_socket import ServerSocket
+from socket_wrapper import SocketWrapper
+from threading import Thread
 
 class Server(object):
     """服务器核心类"""
@@ -10,17 +12,45 @@ class Server(object):
 
     def startup(self):
         """获取客户端连接，并提供服务"""
-        #获取客户端连接
-        print('正在获取客户端连接~~')
-        soc,addr = self.server_socket.accept()
-        print('获取到客户端连接~~')
-        #收发消息
-        recv_data=soc.recv(512)
-        print(recv_data.decode('utf-8'))
-        soc.send('成功连接到服务器~~'.encode('utf-8'))
+        while True:
+            #获取客户端连接
+            print('正在获取客户端连接~~')
+            soc,addr = self.server_socket.accept()
+            print('获取到客户端连接~~')
+            #使用套接字生成包装对象
+            client_soc=SocketWrapper(soc)
+            #收发消息 
+            Thread(target=lambda:self.request_handle(client_soc)).start( )
 
-        #关闭客户端套接字
-        soc.close()
+            #关闭客户端套接字
+            #soc.close()
+
+    def request_handle(self,client_soc):
+        """处理客户端请求"""
+        while True:
+            #接受客户端数据
+            recv_data=client_soc.recv_data()
+            if not recv_data:
+                #没有接收到数据客户端应该已经关闭
+                self.remove_offline_user(client_soc)
+                client_soc.close()
+                break
+            #解析数据
+            parse_data=self.parse_request_text(recv_data)
+
+            #分析请求类型，并根据请求类型调用相应的处理函数
+            print('获取到解析后内容%s' % parse_data)
+
+            print(recv_data)
+            client_soc.send_data('服务器接收到的是：'+ recv_data)
+    def remove_offline_user(self,client_soc):
+        """客户端下线的处理"""
+        print("有客户端下线了~~")
+ 
+    def parse_request_text(self,recv_data):
+        """解析客户端发送来的数据"""
+        print('解析客户端数据'+recv_data)
+
 
 if __name__=='__main__':
     Server().startup()
