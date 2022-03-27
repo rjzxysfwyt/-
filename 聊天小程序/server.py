d16915af60d71c4d094778ef8964aaa97b7fc6d4
rg <83681395+rjzxysfwyt@users.noreply.github.com>
2022-03-27 15:58:25 +0800

    Update server.py

--- 聊天小程序/server.py
+++ 聊天小程序/server.py
@@ -1,7 +1,10 @@
 import socket
+from urllib import request
 from server_socket import ServerSocket
 from socket_wrapper import SocketWrapper
 from threading import Thread
+from config import *
+from response_protocol import *
 
 class Server(object):
     """服务器核心类"""
@@ -10,6 +13,18 @@ class Server(object):
         #创建服务器套接字
         self.server_socket=ServerSocket()
 
+        #创建请求的id和方法关联字典
+        self.request_handle_function={}
+        self.register(REQUEST_LOGIN,self.request_login_handle)
+        self.register(REQUEST_CHAT,self.requset_chat_handle)
+
+        #创建保存当前用户的字典
+        self.clients={}
+        
+
+    def register(self,request_id,handle_function):
+        """再次封装消息类型和处理函数"""
+        self.request_handle_function[request_id]=handle_function
     def startup(self):
         """获取客户端连接，并提供服务"""
         while True:
@@ -39,17 +54,69 @@ class Server(object):
             parse_data=self.parse_request_text(recv_data)
 
             #分析请求类型，并根据请求类型调用相应的处理函数
-            print('获取到解析后内容%s' % parse_data)
-
-            print(recv_data)
+           
+            handle_function=self.request_handle_function.get(parse_data['request_id'])
+            if handle_function:
+                handle_function(client_soc,parse_data)
+            #print(recv_data)
             client_soc.send_data('服务器接收到的是：'+ recv_data)
+
     def remove_offline_user(self,client_soc):
         """客户端下线的处理"""
         print("有客户端下线了~~")
  
-    def parse_request_text(self,recv_data):
-        """解析客户端发送来的数据"""
-        print('解析客户端数据'+recv_data)
+    def parse_request_text(self,text):
+        """
+        解析客户端发送来的数据
+        登录消息：0001|username|password
+        聊天信息：0002|username|massages
+        
+        """
+        print('解析客户端数据'+text)
+        request_list=text.split(DELIMITER)
+        #按照类型解析数据
+        request_data={}
+        request_data['request_id']=request_list[0]
+
+        if request_data['request_id']==REQUEST_LOGIN:
+            #用户请求登陆
+            request_data['username']=request_list[1]
+            request_data['password']=request_list[2]
+
+        elif request_data['request_id']==REQUEST_CHAT:
+            #用户请求聊天
+            request_data['username']=request_list[1]
+            request_data['massages']=request_list[2]
+
+        return request_data
+
+    def requset_chat_handle(self):
+        """处理聊天功能"""
+        print('收到聊天信息~~准备处理~~')
+
+    def request_login_handle(self,client_soc,request_data):
+        """处理登录功能"""
+        print('收到登录请求~~准备处理~~')
+        #获取账号密码
+        username=request_data['username']
+        password=request_data['password']
+
+        #检查是否能够登录
+        ret,nickname,username=self.check_user_login(username,password)
+
+        #登陆成功则需要保存当前用户
+        if ret=='1':
+            self.clients[username]={'sock':client_soc,'nickname':nickname}
+
+        #拼接返回给客户端的消息
+        response_text=ResponseProtocol.response_login_result(ret,nickname,username)
+        #把消息发送给客户端
+        client_soc.send_data(response_text)
+
+
+    def check_user_login(self,username,password):
+        """检查用户是否登陆成功，并返回检查结果（0/失败，1/成功），昵称，用户名"""
+        return '1', 'itcast1', 'user1'
 
 
 if __name__=='__main__':
